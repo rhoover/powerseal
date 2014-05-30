f035a363cf9b1193f32690f8fa187337bed4c769
robinHoover <robin.hoover@gmail.com>
2014-05-30 06:14:05 -0400

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package psv2
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'psv2' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'psv2' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'psv2' ), 'psv2', '<a href="http://www.moosedog.io" rel="designer">Robin Hoover</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
